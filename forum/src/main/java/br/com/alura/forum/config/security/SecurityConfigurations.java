3af98abc0e6537ac0f39f24a103008b9a0aa5b82
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-12-11 18:15:53 -0300

    Curso #2: Finalizado

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -59,6 +59,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	//Configuracoes de recursos estticos(js, css, imagens)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-
+	    web.ignoring()
+	        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
 }
