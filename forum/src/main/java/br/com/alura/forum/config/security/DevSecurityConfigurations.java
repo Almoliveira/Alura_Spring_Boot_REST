fa8de33bd4a9e64db422b8f02f3d38437542c3e7
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-12-13 04:32:10 -0300

    Curso #3: Aula 2

---
+++ forum/src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
@@ -0,0 +1,31 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@Profile("dev")
+public class DevSecurityConfigurations extends WebSecurityConfigurerAdapter{
+	
+	
+	//Configurcoes de Autorizacao
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.and().csrf().disable();
+	}
+	
+	//Configuracoes de recursos estticos(js, css, imagens)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring()
+	        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+	}
+}
